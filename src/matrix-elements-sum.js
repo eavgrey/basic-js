e55d2804c3bcb2df50df64b36c819491ed3dd2cc
eavgrey <81044186+eavgrey@users.noreply.github.com>
2023-04-17 20:11:56 +0300

    feat: done 3 tasks

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given matrix, a rectangular matrix of integers,
@@ -16,11 +16,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getMatrixElementsSum(matrix) {
+  let sumArr = [];
+  let columnCount = matrix[0].length;
+  for (let i = 0; i < columnCount; i++) {
+    for (let j = 0; j < matrix.length; j++) {
+      if (matrix[j][i] !== 0) {
+        sumArr.push(matrix[j][i]);
+      } else {
+        break;
+      }
+    }
+  }
+  let result = sumArr.reduce((a, b) => {
+    return a + b;
+  }, 0);
+  return result;
 }
 
 module.exports = {
-  getMatrixElementsSum
+  getMatrixElementsSum,
 };
--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given an array with heights, sort them except if the value is -1.
@@ -11,11 +11,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  let numbersForSort = [];
+  let sortIndexes = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== -1) {
+      numbersForSort.push(arr[i]);
+      sortIndexes.push(i);
+    }
+  }
+  numbersForSort.sort((a, b) => a - b);
+  for (let j = 0; j < numbersForSort.length; j++) {
+    arr[sortIndexes[j]] = numbersForSort[j];
+  }
+  return arr;
 }
 
 module.exports = {
-  sortByHeight
+  sortByHeight,
 };
